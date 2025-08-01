5edeb2239de93b637b4c7fc7a48992cc5104b985
San4ez-mc <olexandrmatsuk@gmail.com>
2025-08-01 12:57:40 +0300

    Add API error controller returning JSON

--- backend/config/web.php
+++ backend/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'RnmH64wG8bRABrW3rP9QUI0kEDBdcCJz',
+            'enableCsrfValidation' => false,
 
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
@@ -28,7 +29,7 @@ $config = [
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'error/index',
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
@@ -62,7 +63,6 @@ $config = [
                         'POST telegram-login' => 'telegram-login',
                         'POST request-password-reset' => 'request-password-reset',
                         'POST reset-password' => 'reset-password',
-                        'GET csrf' => 'csrf',
                     ],
                 ],
 
@@ -83,7 +83,7 @@ $config = [
                 $response = $event->sender;
                 $response->headers->set('Access-Control-Allow-Origin', '*');
                 $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-                $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token');
+                $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
             },
         ],
         'as cors' => [
